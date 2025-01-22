bb61b2b9fdf3ac4150fa62d7af8a8714a03853e9
Gustavo B. de Souza <114950117+mulack@users.noreply.github.com>
2025-01-22 08:36:14 -0300

    Comentarios kk

--- regua.py
+++ regua.py
@@ -1,31 +1,36 @@
 import random
 
+#pega um valor n e cria n A'd e n B's, dps embaralha
 def estado_inicial(n):
     estado = ['B'] * n + ['A'] * n + ['-']
     random.shuffle(estado)  
     return estado
 
+#verifica se o estado é o estado meta
 def estado_meta(estado, n):
     return estado == ['B'] * n + ['-'] + ['A'] * n
 
+#retorna os movimentos possiveis
 def movimentos_possiveis(estado, posicao_vazia, n):
-    sucessores = []
-    for i in range(len(estado)):
-        if i != posicao_vazia and abs(i - posicao_vazia) <= n:
+    movimentos = []
+    for i in range(len(estado)): #percorre o vetor
+        # se o i não for a posicão do "-" e a distancia entre o i e a vazia (valor abs(positivo)) 
+        # não for maior que n, então o movimento de troca é possivel
+        if i != posicao_vazia and abs(i - posicao_vazia) <= n: 
             novo_estado = estado[:]
-            novo_estado[posicao_vazia], novo_estado[i] = novo_estado[i], '-'
-            sucessores.append(novo_estado)
-    return sucessores
+            novo_estado[posicao_vazia], novo_estado[i] = novo_estado[i], '-' #faz a troca
+            movimentos.append(novo_estado) #adciona na lista dos movimentos
+    return movimentos
 
-
+#como usar
 n = 3
 estado = estado_inicial(n)
 print("Estado Inicial:", estado)
 
 posicao_vazia = estado.index('-')
-sucessores = movimentos_possiveis(estado, posicao_vazia, n)
+movimentos = movimentos_possiveis(estado, posicao_vazia, n)
 print("Movimentos Possíveis:")
-for s in sucessores:
+for s in movimentos:
     print(s)
 
 
